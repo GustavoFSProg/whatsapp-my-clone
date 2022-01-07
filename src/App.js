d3a7edfef3226116381675246c141c58af42821c
Gustavo <gustavo.prog40@gmail.com>
2022-01-07 16:29:49 -0300

    finalmente

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './App.css'
 import Avatar from '../src/assets/avatar.png'
 import Donut from '@material-ui/icons/DonutLarge'
